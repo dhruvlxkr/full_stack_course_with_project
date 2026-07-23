2404613b1fd124b5316a0c58028daf0f053688dd
dhruvlxkr <dhruvlxkr123@gmail.com>
2026-07-23 18:06:22 +0530

    changes

--- javascript/project/Food_recipe_api/index.js
+++ javascript/project/Food_recipe_api/index.js
@@ -1,6 +1,11 @@
 let food = document.querySelector('.food');
 
 let foodapi = async (area) => {
-   const api = fetch(`www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
-   data = 
-} 
+   const api
+      = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
+   const data = await api.json();
+   // console.log(data.meals);
+   return data.meals;
+}
+
+foodapi('india');
